531d3dc69cde8725a481620591f643acef5740bf
Daria Bednarz <dariab12345@gmail.com>
2022-06-27 22:11:13 +0200

    Added all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,301 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from 'react';
+import { Button } from 'react-bootstrap';
+import DateTimeDisplay from './components/DateTimeDisplay';
+import { useCountdown } from './components/useCountdown';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+  const questions = [
+    {
+      questionText: 'Gdy widzisz pół szklanki wody, myślisz: "Ta szklanka jest prawie pełna"',
+      answerOptions: [
+        { answerText: 'Tak', answer: 'TAK' },
+        { answerText: 'Nie', answer: 'NIE' },
+      ]
+    },
+    {
+      questionText: 'Jesteś niecierpliwy i irytuje cię bezczynne wyczekiwanie.',
+      answerOptions: [
+        { answerText: 'Tak', answer: 'TAK' },
+        { answerText: 'Nie', answer: 'NIE' },
+      ]
+    },
+    {
+      questionText: 'Podczas pracy uwielbiasz mieć porządek.',
+      answerOptions: [
+        { answerText: 'Tak', answer: 'TAK' },
+        { answerText: 'Nie', answer: 'NIE' },
+      ]
+    },
+    {
+      questionText: 'Często zastanawiasz się nad sensem życia.',
+      answerOptions: [
+        { answerText: 'Tak', answer: 'TAK' },
+        { answerText: 'Nie', answer: 'NIE' },
+      ]
+    },
+    {
+      questionText: 'Często reagujesz na daną sytuacje zbyt szybko niż powinieneś.',
+      answerOptions: [
+        { answerText: 'Tak', answer: 'TAK' },
+        { answerText: 'Nie', answer: 'NIE' },
+      ]
+    },
+  ];
+
+  const questions_main = [
+    {
+      questionText: '2 + 2 = ?',
+      answerOptions: [
+        { answerText: 'A: 5', isCorrect: false },
+        { answerText: 'B: 4', isCorrect: true },
+        { answerText: 'C: 6', isCorrect: false },
+        { answerText: 'D: 0', isCorrect: false },
+      ]
+    },
+    {
+      questionText: '1 - 1 + 2 = ?',
+      answerOptions: [
+        { answerText: 'A: 3', isCorrect: false },
+        { answerText: 'B: -1', isCorrect: false },
+        { answerText: 'C: 2', isCorrect: true },
+        { answerText: 'D: 1', isCorrect: false },
+      ]
+    },
+    {
+      questionText: '5 - 8 = ?',
+      answerOptions: [
+        { answerText: 'A: 3', isCorrect: false },
+        { answerText: 'B: 2', isCorrect: false },
+        { answerText: 'C: -3', isCorrect: true },
+        { answerText: 'D: -2', isCorrect: false },
+      ]
+    },
+    {
+      questionText: '11 + 88 = ?',
+      answerOptions: [
+        { answerText: 'A: 90', isCorrect: false },
+        { answerText: 'B: 99', isCorrect: true },
+        { answerText: 'C: 100', isCorrect: false },
+        { answerText: 'D: 89', isCorrect: false },
+      ]
+    },
+    {
+      questionText: '7 * 8 = ?',
+      answerOptions: [
+        { answerText: 'A: 42', isCorrect: false },
+        { answerText: 'B: 56', isCorrect: true },
+        { answerText: 'C: 48', isCorrect: false },
+        { answerText: 'D: 46', isCorrect: false },
+      ]
+    },
+    {
+      questionText: '5 * 6 = ?',
+      answerOptions: [
+        { answerText: 'A: 35', isCorrect: false },
+        { answerText: 'B: 42', isCorrect: false },
+        { answerText: 'C: 48', isCorrect: false },
+        { answerText: 'D: 30', isCorrect: true },
+      ]
+    },
+    {
+      questionText: '5 + 5 - 2 = ?',
+      answerOptions: [
+        { answerText: 'A: 12', isCorrect: false },
+        { answerText: 'B: 8', isCorrect: true },
+        { answerText: 'C: 6', isCorrect: false },
+        { answerText: 'D: 7', isCorrect: false },
+      ]
+    },
+    {
+      questionText: '5 - 8 + 3 = ?',
+      answerOptions: [
+        { answerText: 'A: 3', isCorrect: false },
+        { answerText: 'B: -3', isCorrect: false },
+        { answerText: 'C: 0', isCorrect: true },
+        { answerText: 'D: -5', isCorrect: false },
+      ]
+    },
+    {
+      questionText: '6 / 3 = ?',
+      answerOptions: [
+        { answerText: 'A: 2', isCorrect: true },
+        { answerText: 'B: 3', isCorrect: false },
+        { answerText: 'C: 6', isCorrect: false },
+        { answerText: 'D: 18', isCorrect: false },
+      ]
+    },
+    {
+      questionText: '12 / -3 = ?',
+      answerOptions: [
+        { answerText: 'A: -4', isCorrect: true },
+        { answerText: 'B: 4', isCorrect: false },
+        { answerText: 'C: 3', isCorrect: false },
+        { answerText: 'D: -3', isCorrect: false },
+      ]
+    },
+  ];
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [currentQuestion2, setCurrentQuestion2] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [showScore2, setShowScore2] = useState(false);
+  const [showTest, setShowTest] = useState(false);
+  const [answers, setAnswers] = useState([]);
+  const [answers2, setAnswers2] = useState(0);
+  const [personality, setPersonality] = useState('');
+
+  const FIVE_SECONDS = 5 * 1000;
+  const NOW_IN_MS = new Date().getTime();
+
+  const dateTimeAfter = NOW_IN_MS + FIVE_SECONDS;
+  
+  const handleAnswerOptionClick = (answer) => {
+    setAnswers([...answers, answer])
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length){
+      setCurrentQuestion(nextQuestion);
+    } else {
+      getPersonality(answers);
+      setShowScore(true);
+    }
+  };
+
+  const handleAnswerOptionClick2 = (answer) => {
+    setAnswers2(answers2 + answer)
+    const nextQuestion = currentQuestion2 + 1;
+    if (nextQuestion < questions_main.length) {
+      setCurrentQuestion2(nextQuestion);
+    } else {
+      setShowScore2(true);
+    }
+  };
+
+  const handleGoOptionClick = () => {
+    setShowTest(true);
+  }
+
+  const getPersonality = ans => {
+    if (ans[0] === 'TAK' && ans[1] === 'NIE' && ans[3] === 'TAK' && ans[4] === "TAK")
+      setPersonality("Powolny");
+    else if (ans[1] === 'TAK' && ans[2] === 'NIE' && ans[3] === 'NIE')
+      setPersonality("Impulsywny");
+    else if (ans[0] === 'TAK' && ans[2] === 'TAK' && ans[3] === 'TAK')
+      setPersonality("Myśliciel");
+    else if (ans[0] === 'TAK' && ans[1] === 'NIE' && ans[2] === 'TAK' && ans[3] === 'TAK' && ans[4] === 'NIE')
+      setPersonality("Optymista");
+    else
+      setPersonality("Nie określona");
+  }
+
+  const ExpiredNotice = () => {
+    setAnswers2(answers2 - 1);
+    const nextQuestion = currentQuestion2 + 1;
+    if (nextQuestion < questions_main.length) {
+      setCurrentQuestion2(nextQuestion);
+    } else {
+      setShowScore2(true);
+    }
+    return (
+      <div className="expired-notice">
+        <span>Czas minął</span>
+      </div>
+    );
+  };
+  
+  const ShowCounter = ({ days, hours, minutes, seconds }) => {
+    return (
+      <div className="show-counter">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://tapasadhikary.com"
           target="_blank"
           rel="noopener noreferrer"
+          className="countdown-link"
         >
-          Learn React
+          <DateTimeDisplay value={days} type={'Days'} isDanger={days <= 3} />
+          <p>:</p>
+          <DateTimeDisplay value={hours} type={'Hours'} isDanger={false} />
+          <p>:</p>
+          <DateTimeDisplay value={minutes} type={'Mins'} isDanger={false} />
+          <p>:</p>
+          <DateTimeDisplay value={seconds} type={'Seconds'} isDanger={false} />
         </a>
-      </header>
-    </div>
-  );
-}
+      </div>
+    );
+  };
+  
+  const CountdownTimer = ({ targetDate }) => {
+    const [days, hours, minutes, seconds] = useCountdown(targetDate);
+  
+    if (days + hours + minutes + seconds <= 0) {
+      return <ExpiredNotice />;
+    } else {
+      return (
+        <ShowCounter
+          days={days}
+          hours={hours}
+          minutes={minutes}
+          seconds={seconds}
+        />
+      );
+    }
+  };
 
+  return (
+    <div className="App">
+      {showTest ? (
+        <>
+          {showScore2 ? (
+            (answers2 === 0 & answers2 >= 5) ? (<div className="points">Jesteś {personality} Otrzymałeś: {answers2} punktów</div>
+            ) : (
+              (answers2 === 1) ? (<div className="points">Jesteś {personality} Otrzymałeś: {answers2} punkt</div>) : (<div className="points">Jesteś {personality} Otrzymałeś: {answers2} punkty</div>))
+          ) : (
+            <>
+              <div className="question-section">
+                <div className='question-count'>
+                <CountdownTimer targetDate={dateTimeAfter}/>
+                  <span>Pytanie nr {currentQuestion2 + 1}</span>/{questions_main.length}
+                </div>
+                <div className='question-text'>{questions_main[currentQuestion2].questionText}</div>
+              </div>
+              <div className="answer-section">
+                {questions_main[currentQuestion2].answerOptions.map((answerOption, i) => (
+                  <button key={i} onClick={() => handleAnswerOptionClick2(answerOption.isCorrect)}>{answerOption.answerText}</button>
+                ))}
+              </div>
+            </>
+          )}
+        </>
+      ) : (
+        <>
+          {showScore ? (
+            <div className='score-section'>
+              To jest koniec testu, Twoja osobowość to:
+              <div className='row personality'>
+                {personality}
+              </div>
+              <div className='row'>
+                <Button variant="success" onClick={() => handleGoOptionClick()}> Przejdź do testu </Button>
+              </div>
+            </div >
+          ) : (
+            <>
+              <div className="question-section">
+                <div className='question-count'>
+                  <span>Pytanie nr {currentQuestion + 1}</span>/{questions.length}
+                </div>
+                <div className='question-text'>{questions[currentQuestion].questionText}</div>
+              </div>
+              <div className="answer-section">
+                {questions[currentQuestion].answerOptions.map((answerOption, i) => (
+                  <button key={i} onClick={() => handleAnswerOptionClick(answerOption.answer)}>{answerOption.answerText}</button>
+                ))}
+              </div>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  )
+};
 export default App;
